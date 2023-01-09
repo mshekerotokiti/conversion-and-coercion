2b16600d429e56600e3dfeed93618a103674ff90
mshekerotokiti <sharontokiti@gmail.com>
2023-01-09 07:08:38 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//type conversion
+const foodName = 'chapati';
+const foodEat = 'chapati' + 'dengu' //concatenate the strings
+console.log(foodEat);
+const birthYear = 1999;
+const currentYear = Number (birthYear + 24);
+console.log(currentYear)
+console.log(Number ('sharon'));
+//type coercion
+console.log (Number ('21'-'11'+5));
+console.log( 'I am '+ 23 + ' years old');
+console.log( 50-45+'10' )
